7df21d03fd719602e247a70d9f67639ebde2e870
Shirley Zhang <zhangshirleyemail@gmail.com>
2020-06-01 22:55:43 -0400

    set up rooms

--- backend/index.js
+++ backend/index.js
@@ -8,11 +8,38 @@ var io = require('socket.io')(server);
 
 app.use(express.static('.'));
 
+let rooms = {}; 
+
 io.on('connection', (socket) => {
     console.log("connected to server " + socket.id);
     socket.on('disconnect', () => {
         console.log("user disconnected " + socket.id);
     });
+
+    //creates game room, data should contain "room" key
+    socket.on('createRoom', (data) => {
+        if(!rooms.find(data.room)){
+                socket.join(data.room);
+                rooms.push({roomName: 1});
+                console.log("joined new room:", data.room);
+        } else {
+            socket.emit('createRoomFailed', 
+            { message: 'Please choose another name for the room'})
+        }
+    });
+
+    //joins game room
+    socket.on('joinGame', (data) => {
+        if(rooms.find(data.room)) {
+            socket.join(data.room)
+        } else {
+            socket.emit('joinRoomFailed', { message: "Wrong room name"})
+        }
+    })
+
 });
+
+
+
 
 
